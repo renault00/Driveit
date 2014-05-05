cfef3c7c5877307a3f4b968de0509780330624cd
Nkhosisivile Dlamini <village2209@yahoo.co.uk>
2014-05-05 18:06:27 +0200

    Finish user signUP

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -14,8 +15,8 @@ class User < ActiveRecord::Base
 
   before_save { |user| user.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  VALID EMAIL REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID EMAIL REGEX },
-			uniqueness: { case sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+			uniqueness: { case_sensitive: false }
 
 end
