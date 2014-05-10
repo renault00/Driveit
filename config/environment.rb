8e5100be363f191a9e56e220f71d4a8e604733d4
village <village@village-HP-ProBook-6450b.(none)>
2014-05-10 20:10:39 +0200

    configure tls

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 SampleApp::Application.initialize!
 
-
+require 'tlsmail'
+Net::SMTP.enable_tls( OpenSSL::SSL::VERIFY_NONE)
