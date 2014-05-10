2f08841df4c6ecaa5366803979d9efaa3409d7f9
village <village@village-HP-ProBook-6450b.(none)>
2014-05-10 20:07:13 +0200

    Add ambethia-smtp-tls

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 SampleApp::Application.initialize!
 
-require 'smtp-tls'
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-   :address => "smtp.gmail.com",
-   :port => 587,
-   :domain => "gmail.com",
-   :authentication => :plain,
-   :user_name => "driveritauto",  # don't put "@gmail.com" here, just your username
-   :password => "$Driver!t",
-   :enable_starttls_auto => true }
