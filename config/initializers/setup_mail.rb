4d53a5fec634649795d528f7727be976316b7042
village <village@village-HP-ProBook-6450b.(none)>
2014-05-10 20:04:15 +0200

    remove setup mail.rb

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-	:tls => true,
-	:address 			=> "smtp.gmail.com",
-	:port				=> 587,
-	:domain				=> "",
-	:authenticatio		=> :plain,
-	:user_name			=> "driveritauto@gmail.com",
-	:password			=> "$Driver!t"
-}
